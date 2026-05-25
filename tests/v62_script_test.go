0878c7d09551643e8b974bd7ecec009ff985e816
Masaki Komagata <komagata@gmail.com>
2026-05-25 19:22:15 +0900

    Stabilize release gate fixtures

--- tests/v62_script_test.go
+++ tests/v62_script_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"os"
 	"path/filepath"
 	"testing"
 
@@ -20,6 +21,12 @@ func TestV62Scripts(t *testing.T) {
 			env.Setenv("REPO", repo)
 			env.Setenv("GOCACHE", goCache)
 			env.Setenv("GOMODCACHE", modCache)
+			if zig := os.Getenv("TYA_ZIG"); zig != "" {
+				env.Setenv("TYA_ZIG", zig)
+			}
+			if zigDir := os.Getenv("TYA_ZIG_DIR"); zigDir != "" {
+				env.Setenv("TYA_ZIG_DIR", zigDir)
+			}
 			return nil
 		},
 	})
